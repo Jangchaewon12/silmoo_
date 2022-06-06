aaaf0c030c8aa964c36d2f5de74c9d0dacbf09ca
yein_m <yedingshin@gmail.com>
2022-06-06 20:30:06 +0900

    20220606(20:29)

--- yeab_app_reserve_person.java
+++ yeab_app_reserve_person.java
@@ -11,7 +11,9 @@ import android.widget.TextView;
 
 public class yeab_app_reserve_person extends AppCompatActivity {
 
-    boolean timeResA, timeResB, timeResC, timeResD;
+    // 처음부터 끝까지 '민예인' 작성
+
+    /*변수 선언*/
     Intent intent;
     Button btn_1person, btn_2person, btn_3person;
 
@@ -20,65 +22,107 @@ public class yeab_app_reserve_person extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yeab_app_reserve_person);
 
+        /*yeab_app_reserve_time에서 무슨 시간대를 선택했는지 모르니까 다 전달받음*/
         intent = getIntent();
         String rt_time_A = intent.getStringExtra("code_rt_time_A");
-        TextView t_resCodeTimeA = (TextView) findViewById(R.id.t_resCodeTimeA);
+        /*코드를 사용해 'A' 값을 전달받고 그 값을 String rt_time_A에 저장*/
+        TextView t_resCodeTimeA = findViewById(R.id.t_resCodeTimeA);
         t_resCodeTimeA.setText(rt_time_A);
+        /*A타임이 출력될 텍스트뷰 자리 t_resCodeTimeA에 'A' 값을 저장한 rt_time_A을 출력*/
 
         intent = getIntent();
         String rt_time_B = intent.getStringExtra("code_rt_time_B");
-        TextView t_resCodeTimeB = (TextView) findViewById(R.id.t_resCodeTimeB);
+        TextView t_resCodeTimeB = findViewById(R.id.t_resCodeTimeB);
         t_resCodeTimeB.setText(rt_time_B);
 
         intent = getIntent();
         String rt_time_C = intent.getStringExtra("code_rt_time_C");
-        TextView t_resCodeTimeC = (TextView) findViewById(R.id.t_resCodeTimeC);
+        TextView t_resCodeTimeC = findViewById(R.id.t_resCodeTimeC);
         t_resCodeTimeC.setText(rt_time_C);
 
         intent = getIntent();
         String rt_time_D = intent.getStringExtra("code_rt_time_D");
-        TextView t_resCodeTimeD = (TextView) findViewById(R.id.t_resCodeTimeD);
+        TextView t_resCodeTimeD = findViewById(R.id.t_resCodeTimeD);
         t_resCodeTimeD.setText(rt_time_D);
 
+        /*1명을 누르면 yeab_app_reserve_seat_page1으로 넘어감*/
         btn_1person = findViewById(R.id.btn_1person);
         btn_1person.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(yeab_app_reserve_person.this, yeab_app_reserve_seat_page1.class);
-                if (v.getId() == R.id.btn_timeA){
+
+                intent.putExtra("code_person1", "1");
+                /*예약 코드 출력을 위해 '1' 값 전달*/
+
+                /*yeab_app_reserve_time에서 버튼 클릭 이벤트로 값을 putExtra()하여 전달하게끔 했으니 선택하지 않은 시간대의 값은 null*/
+                /*즉, null 값이 아닌 것은 이전 페이지에서 선택한 시간대라는 것을 의미함*/
+                /*null 값이 아니면(이전에 선택한 시간대라면) 그 시간대를 다음 페이지에 또 전달해라(예약 코드를 출력하기 위함)*/
+                if(rt_time_A != null){
                     intent.putExtra("code_rt_time_A", "A");
                 }
-                if(timeResA = true){
-                    intent.putExtra("code_rt_time_A", "A");
-                }
-                if(timeResB = true){
+                else if(rt_time_B != null){
                     intent.putExtra("code_rt_time_B", "B");
                 }
-                if(timeResC = true){
+                else if(rt_time_C != null){
                     intent.putExtra("code_rt_time_C", "C");
                 }
-                if(timeResD = true){
+                else if(rt_time_D != null){
                     intent.putExtra("code_rt_time_D", "D");
                 }
+
                 startActivity(intent);
             }
-
         });
 
+        /*2명을 누르면 yeab_app_reserve_seat_page2로 넘어감*/
         btn_2person = findViewById(R.id.btn_2person);
         btn_2person.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(yeab_app_reserve_person.this, yeab_app_reserve_seat_page2.class);
+                intent = new Intent(yeab_app_reserve_person.this, yeab_app_reserve_seat_page2.class);
+
+                intent.putExtra("code_person2", "2");
+
+                if(rt_time_A != null){
+                    intent.putExtra("code_rt_time_A", "A");
+                }
+                else if(rt_time_B != null){
+                    intent.putExtra("code_rt_time_B", "B");
+                }
+                else if(rt_time_C != null){
+                    intent.putExtra("code_rt_time_C", "C");
+                }
+                else if(rt_time_D != null){
+                    intent.putExtra("code_rt_time_D", "D");
+                }
+
                 startActivity(intent);
             }
         });
 
+        /*3명을 누르면 yeab_app_reserve_seat_page3로 넘어감*/
         btn_3person = findViewById(R.id.btn_3person);
         btn_3person.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(yeab_app_reserve_person.this, yeab_app_reserve_seat_page3.class);
+                intent = new Intent(yeab_app_reserve_person.this, yeab_app_reserve_seat_page3.class);
+
+                intent.putExtra("code_person3", "3");
+
+                if(rt_time_A != null){
+                    intent.putExtra("code_rt_time_A", "A");
+                }
+                else if(rt_time_B != null){
+                    intent.putExtra("code_rt_time_B", "B");
+                }
+                else if(rt_time_C != null){
+                    intent.putExtra("code_rt_time_C", "C");
+                }
+                else if(rt_time_D != null){
+                    intent.putExtra("code_rt_time_D", "D");
+                }
+
                 startActivity(intent);
             }
         });
--- yeab_app_reserve_time.java
+++ yeab_app_reserve_time.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.textclassifier.SelectionEvent;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 
 public class yeab_app_reserve_time extends AppCompatActivity {
 
-    public Boolean timeResA, timeResB, timeResC, timeResD = false;
+    // 처음부터 끝까지 '민예인' 작성
+
+    /*변수 선언*/
     Intent intent;
     public Button btn_timeA, btn_timeB, btn_timeC, btn_timeD;
     ImageButton Ibtn_before;
@@ -21,63 +24,63 @@ public class yeab_app_reserve_time extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yeab_app_reserve_time);
 
+        /*A타임 버튼 누르면 다음 페이지로 전환*/
         btn_timeA = findViewById(R.id.btn_timeA);
         btn_timeA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(timeResA = true){
-                    intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
-                    intent.putExtra("code_rt_time_A", "A");
-                    startActivity(intent);
-                }
+                intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
+                intent.putExtra("code_rt_time_A", "A");
+                /*예약 코드 출력을 위해 'A' 값을 코드를 사용해 전달함*/
+                /*이 코드를 입력하면 'A'(value)가 출력됨*/
+                startActivity(intent);
             }
         });
 
+        /*B타임 버튼 누르면 다음 페이지로 전환*/
         btn_timeB = findViewById(R.id.btn_timeB);
         btn_timeB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(timeResB = true){
-                    intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
-                    intent.putExtra("code_rt_time_B", "B");
-                    startActivity(intent);
-                }
+                intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
+                intent.putExtra("code_rt_time_B", "B");
+                startActivity(intent);
             }
         });
 
+        /*C타임 버튼 누르면 다음 페이지로 전환*/
         btn_timeC = findViewById(R.id.btn_timeC);
         btn_timeC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(timeResC = true){
-                    intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
-                    intent.putExtra("code_rt_time_C", "C");
-                    startActivity(intent);
-                }
+                intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
+                intent.putExtra("code_rt_time_C", "C");
+                startActivity(intent);
             }
         });
 
+        /*D타임 버튼 누르면 다음 페이지로 전환*/
         btn_timeD = findViewById(R.id.btn_timeD);
         btn_timeD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(timeResD = true){
-                    intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
-                    intent.putExtra("code_rt_time_D", "D");
-                    startActivity(intent);
-                }
+                intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_person.class);
+                intent.putExtra("code_rt_time_D", "D");
+                startActivity(intent);
             }
         });
 
+        /*이전 버튼(누르면 yeab_app_reserve_02로 돌아감)*/
         Ibtn_before = findViewById(R.id.Ibtn_before);
         Ibtn_before.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
-                Intent intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_02.class);
+                intent = new Intent(yeab_app_reserve_time.this, yeab_app_reserve_02.class);
                 startActivity(intent);
             }
         });
 
     }
+
 }
